95794c4775c9f72dca936b20da3af3ca32c89fd2
Kuljit Singh <ksingh@cs.csubak.edu>
2019-01-23 13:54:23 -0800

    changed hardcoded id to 'default'

--- sql/mockData/salesperson.sql
+++ sql/mockData/salesperson.sql
@@ -1,11 +1,11 @@
 INSERT INTO "salesperson" VALUES
-    (1,'c8dd53','Alexandra','Jenoure','1999-11-26 00:00:00','78296 Green Junction','Bakersfield','CA',93301,'f','562-25-1842'),
-    (2,'fd815a','Joelie','Lakenton','1958-04-28 00:00:00','0 Calypso Junction','Bakersfield','CA',93305,'f','789-36-6950'),
-    (3,'916113','Jojo','Dodswell','1953-12-21 00:00:00','414 Northridge Point','Bakersfield','CA',93311,'m','686-46-4410'),
-    (4,'813fe3','Jazmin','Praton','1989-01-06 00:00:00','80 John Wall Drive','Bakersfield','CA',93301,'f','527-53-9830'),
-    (5,'1c83ce','Quincy','Luppitt','1988-01-06 00:00:00','73 Killdeer Crossing','Bakersfield','CA',93302,'m','847-82-9625'),
-    (6,'beced6','Lea','Nutley','1977-07-10 00:00:00','7872 Homewood Trail','Bakersfield','CA',93307,'f','755-23-3201'),
-    (7,'6181d0','Minda','Cutmare','1994-04-06 00:00:00','9271 Farmco Way','Bakersfield','CA',93301,'f','180-66-1113'),
-    (8,'b76e15','Leoine','Farenden','1954-05-15 00:00:00','0 American Ash Street','Bakersfield','CA',93311,'m','211-74-0797'),
-    (9,'3b5989','Constantia','Rozsa','1959-11-27 00:00:00','1380 Nova Parkway','Bakersfield','CA',93309,'f','409-30-5454'),
-    (10,'8fa7fc','Shaughn','Earney','1969-08-18 00:00:00','6 Pennsylvania Park','Bakersfield','CA',93301,'m','247-11-9891');
+    (default,'c8dd53','Alexandra','Jenoure','1999-11-26 00:00:00','78296 Green Junction','Bakersfield','CA',93301,'f','562-25-1842'),
+    (default,'fd815a','Joelie','Lakenton','1958-04-28 00:00:00','0 Calypso Junction','Bakersfield','CA',93305,'f','789-36-6950'),
+    (default,'916113','Jojo','Dodswell','1953-12-21 00:00:00','414 Northridge Point','Bakersfield','CA',93311,'m','686-46-4410'),
+    (default,'813fe3','Jazmin','Praton','1989-01-06 00:00:00','80 John Wall Drive','Bakersfield','CA',93301,'f','527-53-9830'),
+    (default,'1c83ce','Quincy','Luppitt','1988-01-06 00:00:00','73 Killdeer Crossing','Bakersfield','CA',93302,'m','847-82-9625'),
+    (default,'beced6','Lea','Nutley','1977-07-10 00:00:00','7872 Homewood Trail','Bakersfield','CA',93307,'f','755-23-3201'),
+    (default,'6181d0','Minda','Cutmare','1994-04-06 00:00:00','9271 Farmco Way','Bakersfield','CA',93301,'f','180-66-1113'),
+    (default,'b76e15','Leoine','Farenden','1954-05-15 00:00:00','0 American Ash Street','Bakersfield','CA',93311,'m','211-74-0797'),
+    (default,'3b5989','Constantia','Rozsa','1959-11-27 00:00:00','1380 Nova Parkway','Bakersfield','CA',93309,'f','409-30-5454'),
+    (default,'8fa7fc','Shaughn','Earney','1969-08-18 00:00:00','6 Pennsylvania Park','Bakersfield','CA',93301,'m','247-11-9891');
