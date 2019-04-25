2b1ba033596d70be1b631ac2910413d5880a2ea7
Joshua Cancellier <jcancellier@csub.edu>
2019-04-25 03:07:18 -0700

    added customer post functionality

--- controllers/customer.js
+++ controllers/customer.js
@@ -9,11 +9,46 @@ findAll = (req, res) => {
       }],
       limit: count
     })
-      .then((customers) => { res.status(200).send(customers) })
+      .then((customers) => {res.status(200).send(customers)})
       .catch((err) => res.status(400).send(err))
   );
 }
 
+add = (req, res) => {
+
+  const { 
+    first_name,
+    last_name,
+    drivers_license,
+    dob,
+    issue_date,
+    expiration_date,
+    sex,
+    street,
+    city,
+    state,
+    zipcode
+  } = req.body;
+
+  return Customer
+    .create({
+      first_name,
+      last_name,
+      drivers_license,
+      dob,
+      issue_date,
+      expiration_date,
+      sex,
+      street,
+      city,
+      state,
+      zipcode
+    })
+    .then((customer) => res.status(201).send(customer))
+    .catch((error) => res.status(400).send(error));
+}
+
 module.exports = {
-  findAll
+  findAll,
+  add
 }
--- routes/customer.js
+++ routes/customer.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var controller = require('../controllers').customer;
 
 router.get('/', controller.findAll);
+router.post('/', controller.add);
 
 module.exports = router;
